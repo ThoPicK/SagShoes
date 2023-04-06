9098d8fc78fbbf8d516a5c6d61354d1e721b8ca6
NadaT24 <ntriki@etu.uqac.ca>
2023-04-06 15:52:58 -0400

    Article Nada

--- Article.cpp
+++ Article.cpp
@@ -1,1 +1,24 @@
 #include "Article.h"
+
+Article::Article(int id, int quantite, double taille) : id(id), quantite(quantite), taille(taille) {}
+
+int Article::getId() const {
+	return id; 
+}
+void Article::setId(int id) {
+	this->id = id;
+}
+
+int Article::getQuantite() const { 
+	return quantite;
+}
+void Article::setQuantite(int quantite) {
+	this->quantite = quantite;
+}
+
+double Article::getTaille() const { 
+	return taille;
+}
+void Article::setTaille(double taille) {
+	this->taille = taille;
+}
--- Article.h
+++ Article.h
@@ -1,5 +1,29 @@
 #pragma once
+#ifndef ARTICLE_H
+#define ARTICLE_H
+
+
 class Article
 {
+private:
+	int id;
+	int quantite;
+	int taille;
+
+public:
+	Article();
+	Article(int id, int quantite, double taille);
+
+	int getId()const;
+	void setId(int id);
+
+	int getQuantite() const;
+	void setQuantite(int quantite);
+
+	double getTaille() const;
+	void setTaille(double taille);
+
 };
+
+#endif
 
