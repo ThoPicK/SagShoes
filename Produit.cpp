cd3a2735089b6be85a9a16eed160cadf1189c779
NadaT24 <ntriki@etu.uqac.ca>
2023-04-06 14:46:12 -0400

    produit Nada

--- Produit.cpp
+++ Produit.cpp
@@ -1,3 +1,55 @@
 #include "Produit.h"
+using namespace std;
 
-class Produit // test 
+
+Produit::Produit(int id, int stock, float prix, float prixLivraison, std::string nom, std::string description): ):
+	id(id), stock(stock), prix(prix), prixlivraison(prixLivraison), nom(nom), description(description){}
+
+Produit::Produit()
+{
+}
+
+Produit::Produit(int id, int stock, float prix, float prixLivraison, std::string nom, std::string description)
+{
+}
+
+int Produit::getId()const {
+	return id;
+}
+void Produit::setId(int id) {
+	this->id = id;
+}
+int Produit::getStock() const {
+	return stock; 
+}
+void Produit::setStock(int stock) {
+	this->stock = stock;
+}
+
+float Produit::getPrix() const {
+	return prix; 
+}
+void Produit::setPrix(float prix) {
+	this->prix = prix;
+}
+
+float Produit::getPrixlivraison() const { 
+	return prixlivraison; 
+}
+void Produit::setPrixlivraison(float prixlivraison) { 
+	this->prixlivraison = prixlivraison;
+}
+
+std::string Produit::getNom() const {
+	return nom;
+}
+void Produit::setNom(std::string nom) {
+	this->nom = nom;
+}
+
+std::string Produit::getDescription() const {
+	return description;
+}
+void Produit::setDescription(std::string description) {
+	this->description = description;
+}
--- Produit.h
+++ Produit.h
@@ -1,5 +1,39 @@
-#pragma once
+#ifndef PRODUIT_H
+#define PRODUIT_H
+#include "String"
+
 class Produit
 {
+private :
+	int id;
+	int stock; 
+	float prix;
+	float prixlivraison; 
+	std::string nom;
+	std::string description;
+
+public:
+	Produit();
+	Produit(int id, int stock, float prix, float prixLivraison, std::string nom, std::string description);
+
+	int getId() const;
+	void setId(int id);
+
+	int getStock() const;
+	void setStock(int stock);
+
+	float getPrix() const;
+	void setPrix(float prix);
+
+	float getPrixlivraison() const;
+	void setPrixlivraison(float prixlivraison);
+
+	std::string getNom()const;
+	void setNom(std::string nom);
+
+	std::string getDescription()const;
+	void setDescription(std::string description);
+
 };
+#endif
 
