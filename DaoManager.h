49940435a6db798cb04ffca42c96c1e163568211
Thomas Duhamel <thomas.duhamel@mines-ales.org>
2023-04-09 05:53:00 -0400

    DaoManager avancement

---
+++ DaoManager.h
@@ -0,0 +1,34 @@
+#ifndef DAO_H
+#define DAO_H
+#include <stdlib.h>
+#include <iostream>
+
+#include "User.h"
+
+#include "mysql_connection.h"
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/prepared_statement.h>
+using namespace std;
+using namespace sql;
+
+class DaoManager
+{
+private:
+	DaoManager();
+
+	static DaoManager* instance_;
+	Driver* driver_;
+	Connection* connection_;
+	User* connectedUser_;
+
+public:
+	static DaoManager* getInstance();
+	void connect(string email, string password);
+	void disconnect();
+	User* getConnectedUser();
+	Panier* getPanier(int id);
+
+};
+
+#endif
