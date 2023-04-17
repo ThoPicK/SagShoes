eeb41bf5604c3e708c8a614c4ebf70f8ef296ab6
NadaT24 <ntriki@etu.uqac.ca>
2023-04-17 14:57:27 -0400

    Update dialog.cpp
    dialog.cpp

--- dialog.cpp
+++ dialog.cpp
@@ -1,37 +1,81 @@
-#include<ui_dialog.h>
-#include "Produit.h"
-#include <vector>
-#include <QTableWidget>
-#include <QLabel>
-#include <QDialog>
-#include"ui_dialog.h"
+#include "dialog.h"
+#include "ui_dialog.h"
+#include<QMessageBox>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QHeaderView>
+#include <QTableWidgetItem>
 
-namespace Ui {
-class Dialog;
+
+Dialog::Dialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::Dialog)
+{
+    tableWidget = new QTableWidget(this);
+    tableWidget->setColumnCount(5);
+    tableWidget->setHorizontalHeaderLabels({"ID", "Nom", "Prix", "Prix Livraison", "Description"});
+    tableWidget->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+    connect(tableWidget, &QTableWidget::itemSelectionChanged, [this]() {
+        auto selectedItems = tableWidget->selectedItems();
+        if (!selectedItems.isEmpty()) {
+            idLabel->setText(selectedItems[0]->text());
+            nomLabel->setText(selectedItems[1]->text());
+            prixLabel->setText(selectedItems[2]->text());
+            prixLivraisonLabel->setText(selectedItems[3]->text());
+            descriptionLabel->setText(selectedItems[4]->text());
+        }
+    });
+
+    idLabel = new QLabel(this);
+    nomLabel = new QLabel(this);
+    prixLabel = new QLabel(this);
+    prixLivraisonLabel = new QLabel(this);
+    descriptionLabel = new QLabel(this);
+
+    QVBoxLayout *labelsLayout = new QVBoxLayout();
+    labelsLayout->addWidget(new QLabel("ID:"));
+    labelsLayout->addWidget(idLabel);
+    labelsLayout->addWidget(new QLabel("Nom:"));
+    labelsLayout->addWidget(nomLabel);
+    labelsLayout->addWidget(new QLabel("Prix:"));
+    labelsLayout->addWidget(prixLabel);
+    labelsLayout->addWidget(new QLabel("Prix Livraison:"));
+    labelsLayout->addWidget(prixLivraisonLabel);
+    labelsLayout->addWidget(new QLabel("Description:"));
+    labelsLayout->addWidget(descriptionLabel);
+    labelsLayout->addStretch();
+
+    QHBoxLayout *mainLayout = new QHBoxLayout(this);
+    mainLayout->addWidget(tableWidget);
+    mainLayout->addLayout(labelsLayout);
 }
 
-class Dialog : public QDialog
+void Dialog::setProduit(const std::vector<Produit> &produits)
 {
-    Q_OBJECT
+    tableWidget->setRowCount(produits.size());
 
-public:
-    explicit Dialog(QWidget *parent = nullptr);
-    void setProduit(const std::vector<Produit> &produits);
-    ~Dialog();
+    for (size_t i = 0; i < produits.size(); ++i) {
+        const auto &produit = produits[i];
 
-private:
-
-    Ui::Dialog *ui;
-    QTableWidget *tableWidget;
-    QLabel *idLabel;
-    QLabel *nomLabel;
-    QLabel *prixLabel;
-    QLabel *prixLivraisonLabel;
-    QLabel *descriptionLabel;
-};
-
-#endif // PRODUCTTABLEWIDGET_H
-    // DIALOG_H
+        tableWidget->setItem(i, 0, new QTableWidgetItem(QString::number(produit.getId())));
+        tableWidget->setItem(i, 1, new QTableWidgetItem(QString::fromStdString(produit.getNom())));
+        tableWidget->setItem(i, 2, new QTableWidgetItem(QString::number(produit.getPrix())));
+        tableWidget->setItem(i, 3, new QTableWidgetItem(QString::number(produit.getPrixlivraison())));
+        tableWidget->setItem(i, 4, new QTableWidgetItem(QString::fromStdString(produit.getDescription())));
+    }
+}
 
 
+Dialog::~Dialog()
+{
+
+}
+
+
+
+/*void Dialog::on_pushButton_clicked()
+{
+
+}
+*/
 
